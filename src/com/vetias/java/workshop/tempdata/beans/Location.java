fd7a1c4ac7eca93dde75717516be5e6bdfd3ca51
Hari291120 <hariharang24it@vetias.ac.in>
2025-07-04 16:03:43 +0530

    Location

--- src/com/vetias/java/workshop/tempdata/beans/Location.java
+++ src/com/vetias/java/workshop/tempdata/beans/Location.java
@@ -7,8 +7,44 @@ public class Location {
     private String country;
     private String city;
     private String address;
-    private String postalcode;
+    public void setName(String aName ){
+        name = aName; }
+      public String getname() {
+        return name;
+    }
 
-    public void setName(String )
-      
-} 
+      public void setlatitude(double alatitude){
+          latitude = alatitude;
+    }
+        public double getlatitude(){
+          return latitude;
+    } 
+        public void setlongtitude(double along){
+            longitude=along;
+    }
+        public double getlongtitude(){
+            return longitude;
+    }
+       public void setdescrription( String des){
+         description = des;
+       }
+
+          public String getdescription() {
+            return description;
+    }
+          public void setaddress(String addresses) {
+            address = addresses;
+    }
+          public String getaddress(String address){
+             return address;
+    }          
+@Override
+          public String toString(){
+            return"Location{"+
+                  "name:'"+ name + '\''+
+                  ",latitude;"+ latitude +
+                  ", longitude:"+ longitude +
+                  ",address:'"+ address +'\''+
+                '}';
+            }
+          }
